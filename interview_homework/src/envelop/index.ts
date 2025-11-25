458a11cbf33f2bf89c497641fdcae2b4d3be1329
prashanth <pvprashanth030@gmail.com>
2025-11-25 09:37:06 -0600

    TICKET 5 and 6: updated the logger and appended the requestId from the context

--- interview_homework/src/envelop/index.ts
+++ interview_homework/src/envelop/index.ts
@@ -4,14 +4,17 @@ import { useParserCache } from '@envelop/parser-cache';
 import { useValidationCache } from '@envelop/validation-cache';
 import { buildHeaders } from './buildHeaders';
 import { useLogger } from './useLogger';
+import { appendRequestId } from './appendRequestId';
 import { ContextType } from '../types';
 
 const plugins: Plugin<ContextType>[] = [
   useEngine({ parse, validate, specifiedRules, execute, subscribe }) as Plugin<ContextType>,
   buildHeaders(),
   useLogger(),
+  appendRequestId(),   // <-- here
   useParserCache() as Plugin<ContextType>,
   useValidationCache() as Plugin<ContextType>,
 ];
+
 
 export default plugins;
